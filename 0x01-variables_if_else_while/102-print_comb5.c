b59d9f90b0b2f5992db4d6352f827690faf8ea2f
George Raafat <georgexraafat@gmail,com>
2023-03-17 15:54:01 -0700

    a

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+int j;
+int k;
+int x;
+
+for (i = 48; i < 58; i++)
+{
+for (j = 48; j < 58; j++)
+{
+for (k = 48; k < 58; k++)
+{
+for (x = 48; x < 58; x++)
+{
+if (x <= j && k <= i)
+continue;
+putchar (i);
+putchar (j);
+putchar (' ');
+putchar (k);
+putchar (x);
+if (i == '9' && j == '8' && k == '9' && x == '9')
+{
+putchar ('\n');
+break;
+}
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+return (0);
+}
