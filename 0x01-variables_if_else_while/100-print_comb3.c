65cc84a5fd434e410891349341f8005a0aa01dce
George Raafat <georgexraafat@gmail,com>
2023-03-17 15:16:25 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+int j;
+
+for (i = 48; i < 57; i++)
+{
+for (j = i + 1; j < 58; j++)
+{
+putchar (i);
+putchar (j);
+if (i == 56 && j == 57)
+{
+putchar ('\n');
+break;
+}
+putchar (',');
+putchar (' ');
+}
+}
+return (0);
+}
