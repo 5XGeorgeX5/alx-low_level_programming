e3081c48a6c6e2fc0e4aa72511b4c89b515a6523
George Raafat <georgexraafat@gmail,com>
2023-03-21 04:46:55 -0700

    a

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: The character to checked
+ *
+ * Return: 1 if alpha, 0 if not
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'Z' && c <= 'Z'))
+return (1);
+return (0);
+}
