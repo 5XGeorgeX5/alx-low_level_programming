6e7eda36a4dd64661893023cb1a2a636db0ed5df
George Raafat <georgexraafat@gmail,com>
2023-03-21 05:05:23 -0700

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: The character to checked
+ *
+ * Return: the absolute value of n
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+return (n * -1);
+return (n);
+}
