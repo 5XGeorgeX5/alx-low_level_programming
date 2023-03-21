68c128b2177ff57926a28f080f720f54dd510819
George Raafat <georgexraafat@gmail,com>
2023-03-21 04:09:01 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c
+
+        for (c = 'a'; c <= 'z'; i++)
+                _putchar(c);
+	_putchar('\n');
+        return;
+}
