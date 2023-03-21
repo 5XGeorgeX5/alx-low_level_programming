e0773945503712eae5576295ee11f15d2ffa36e5
George Raafat <georgexraafat@gmail,com>
2023-03-21 04:26:29 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,8 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+int i;
+char c;
 
-	for (i = 0; i < 10; i++)
-		print_alphabet();
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
 }
