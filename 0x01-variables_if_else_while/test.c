2d9430f31b90b8dd20915557abf875d422748c87
George Raafat <georgexraafat@gmail,com>
2023-03-22 23:07:35 -0700

    a

---
+++ 0x01-variables_if_else_while/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+int y;
+
+for (int x = 3; x < 10; x += 2)
+printf("%i\n", x);
+
+return (0);
+}
