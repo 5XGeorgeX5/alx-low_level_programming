c07f7858f520fdad7393c9cf4cf582d5148d3199
George Raafat <georgexraafat@gmail,com>
2023-03-23 14:12:54 -0700

    a

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: the number to be printed
+ *
+ */
+
+void print_number(int n)
+{
+
+int a[10];
+int i;
+
+if (!n)
+{
+_putchar('0');
+_putchar('\n');
+return;
+}
+
+if (n < 0)
+{
+n *= -1;
+_putchar('-');
+}
+i = 0;
+while (n)
+{
+a[i] = n % 10;
+n /= 10;
+i++;
+}
+i--;
+for (; i >= 0; i--)
+{
+_putchar('0' + a[i]);
+}
+_putchar('\n');
+}
