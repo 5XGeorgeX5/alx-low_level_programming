e27dfdbff365fa699663b03d497aa2b4f8d50737
George Raafat <georgexraafat@gmail,com>
2023-03-23 10:25:06 -0700

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line.
+ * @n: the number of times the character _
+ *
+ */
+
+void print_line(int n)
+{
+while (n--)
+_putchar('_');
+_putchar('\n');
+}
