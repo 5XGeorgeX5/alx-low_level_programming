59d8fb8df08d87881755f480075390c6926bea2c
George Raafat <georgexraafat@gmail,com>
2023-03-30 19:02:25 -0700

    a

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+/**
+ * _strcmp - compare 2 strings
+ *
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: comparison result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i, j;
+
+for (i = 0; s1[i]; i++)
+{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+}
+return (s1[i] - s2[i]);
+}
