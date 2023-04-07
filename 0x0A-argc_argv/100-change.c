7520d695da8984b19b6e3d65bd1062d2d8858b9d
George Raafat <georgexraafat@gmail,com>
2023-04-07 05:43:51 -0700

    a

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: the number of inputs
+ * @argv: the array of inputs
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+int a;
+int sum;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+a = atoi(argv[1]);
+sum = a / 25;
+a %= 25;
+sum += a / 10;
+a %= 10;
+sum += a / 5;
+a %= 5;
+sum += a;
+printf("%d\n", sum);
+return (0);
+}
