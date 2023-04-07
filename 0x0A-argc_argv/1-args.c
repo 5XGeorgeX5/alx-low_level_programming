25d02b6b4738934cc8b723366997f3ed735cd8e2
George Raafat <georgexraafat@gmail,com>
2023-04-07 04:49:49 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: the number of inputs
+ * @argv: the array of inputs
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+
+printf("%d\n", argc);
+return (0);
+}
