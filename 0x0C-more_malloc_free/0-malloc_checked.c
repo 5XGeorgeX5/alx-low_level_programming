1a33918ea1ad544116f5a78966f6b33123f63294
George Raafat <georgexraafat@gmail,com>
2023-04-13 16:31:13 -0700

    a

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: the size
+ * 
+ * Return: the pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *s = malloc(b);
+
+if (!s)
+exit(98);
+return (s);
+}
