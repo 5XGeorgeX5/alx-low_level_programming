26cd6e6b914d449ea7502299b173bdc7404d3511
George Raafat <georgexraafat@gmail,com>
2023-04-19 16:16:41 -0700

    a

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a name
+ * @name: The name
+ * @f: the function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef F_H
+#define F_H
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
