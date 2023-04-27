b00ac304ba51bac0873ca8c1fd8d9466d9d8e94e
George Raafat <georgexraafat@gmail.com>
2023-04-27 13:23:06 -0700

    a

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: the list
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
