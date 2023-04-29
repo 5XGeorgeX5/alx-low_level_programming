df63fc61c7ad45481a813d2e5543fa1082454cb5
George Raafat <georgexraafat@gmail.com>
2023-04-29 13:33:53 -0700

    a

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (!head)
+		return;
 	while (head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-	head = NULL;
+	*head = NULL;
 }
