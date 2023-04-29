83dcd783f7040d8c5c0957e8608b2e037efced10
George Raafat <georgexraafat@gmail.com>
2023-04-29 14:00:15 -0700

    a

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,31 +1,6 @@
 #include "lists.h"
 
 /**
- * p_number - print number
- * @n: number to be printed
- *
- */
-
-void p_number(int n)
-{
-	unsigned int n1;
-
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	}
-	else
-		n1 = n;
-
-	if (n1 / 10)
-		p_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
-}
-
-
-/**
  * print_listint - prints all the elements of a listint_t list
  * @h: the list
  *
@@ -39,8 +14,7 @@ size_t print_listint(const listint_t *h)
 	for (; h; h = h->next)
 	{
 		i++;
-		p_number(h->n);
-		_putchar('\n');
+		printf("%i\n", h->n);
 	}
 	return (i);
 }
