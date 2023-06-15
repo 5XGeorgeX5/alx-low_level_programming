b28728b4878daeed055cc0fe8688622cc79748b7
George Raafat <georgexraafat@gmail.com>
2023-06-15 06:42:50 -0700

    a

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: the head of a dlistint_t list
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *p = h;
+	size_t n = 0;
+	while (p)
+	{
+		printf("%d\n", p->n);
+		p = p->next;
+		n++;
+	}
+	return (n);
+}
