c133407bf1f7f4083e3e0634b18cf6b0bd28ccb8
George Raafat <georgexraafat@gmail.com>
2023-06-15 07:21:37 -0700

    a

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	tmp->n = n;
 	tmp->next = *head;
 	tmp->prev = NULL;
-	(*head)->prev = tmp;
+	if (*head)
+		(*head)->prev = tmp;
 	*head = tmp;
 	return (tmp);
 }
