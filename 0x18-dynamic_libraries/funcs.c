21f7a93d245813e82566e8f00c1cc1c7ef81225b
George Raafat <georgexraafat@gmail.com>
2023-06-22 13:27:23 -0700

    a

---
+++ 0x18-dynamic_libraries/funcs.c
@@ -0,0 +1,64 @@
+/**
+ * add - add 2 numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub 2 numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul 2 numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div 2 numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod 2 numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
