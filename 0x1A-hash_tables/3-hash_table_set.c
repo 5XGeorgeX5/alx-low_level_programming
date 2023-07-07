44d46ec5dd2093e98334e56922d571d3233ad3c9
George Raafat <georgexraafat@gmail.com>
2023-07-07 12:52:52 -0700

    a

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,21 +12,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
 	char *value_copy;
-	unsigned long int i;
+	unsigned long int index, i;
 
-	if (!ht || !key || *key == '\0' || !value)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
 
-	i = key_index((const unsigned char *)key, ht->size);
-	if (!strcmp(ht->array[i]->key, key))
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		free(ht->array[i]->value);
-		ht->array[i]->value = value_copy;
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
 	}
 
 	new = malloc(sizeof(hash_node_t));
@@ -39,12 +42,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new->key == NULL)
 	{
 		free(new);
-		free(value_copy);
 		return (0);
 	}
 	new->value = value_copy;
-	new->next = ht->array[i + 1];
-	ht->array[i] = new;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
