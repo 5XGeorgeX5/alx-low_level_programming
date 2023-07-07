2810db69891c3ea6f4e7d273a69935b53f15f6e1
George Raafat <georgexraafat@gmail.com>
2023-07-07 14:14:41 -0700

    a

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *search;
+	unsigned long int i;
+	int comma = 0;
+
+	putchar('{');
+	if (ht)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			search = ht->array[i];
+			while (search)
+			{
+				if (comma)
+					printf(", ");
+				printf("'%s': '%s'", search->key, search->value);
+				search = search->next;
+				comma = 1;
+			}
+		}
+	}
+	printf("}\n");
+}
