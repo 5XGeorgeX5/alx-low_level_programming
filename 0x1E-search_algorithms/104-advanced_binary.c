899a242cbddc96fc14a148a80362f4e21ae2e4c2
George Raafat <georgexraafat@gmail.com>
2023-10-11 15:00:10 -0700

    doing some task

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,33 @@
 #include "search_algos.h"
 
 /**
+ * binary - searches for a value in an array of integers
+ * using the Binary search algorithm
+ *
+ * @array:  pointer to the first element of the array to search in
+ * @left: the start of the array to search
+ * @right: the end of the array to search
+ * @value:  the value to search for
+ * Return: the index of the value
+*/
+int binary(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (left >= right)
+		return ((int)right);
+	printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+	i = (left + right) / 2;
+	if (value <= array[i])
+		return (binary(array, left, i, value));
+	else
+		return (binary(array, i + 1, right, value));
+}
+
+/**
  * advanced_binary - searches for a value in an array of integers
  * using the Binary search algorithm
  *
@@ -11,26 +38,15 @@
 */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1, i;
 	int index;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
-
-	while (left < right)
+	index = binary(array, 0, size - 1, value);
+	if (array[index] != value)
 	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-		i = (left + right) / 2;
-		if (value <= array[i])
-			right = i;
-		else
-			left = i + 1;
+		printf("Searching in array: %d\n", array[index]);
+		return (-1);
 	}
-	index = (array[right] == value) ? (int)right : -1;
-	if (index == -1)
-		printf("Searching in array: %d\n", array[right]);
 	return (index);
 }
